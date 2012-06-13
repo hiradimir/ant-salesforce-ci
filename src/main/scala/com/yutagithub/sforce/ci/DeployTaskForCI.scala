43e10a40d916b6396c4feba099aa99596d0cae8c
yutaka <yutakachang+github@gmail.com>
2012-06-13 16:48:12 +0900

    using target dir for build

--- src/main/scala/com/yutagithub/sforce/ci/DeployTaskForCI.scala
+++ src/main/scala/com/yutagithub/sforce/ci/DeployTaskForCI.scala
@@ -13,9 +13,17 @@ case class DeployTaskForCI extends DeployTask {
     this.setRollbackOnError(true);
     // for all deploy
     this.setAutoUpdatePackage(true);
-    //if (this.getProject().getProperty("sfc.sobjectPlural") == true.toString) {
+    
+    var presentDir = new java.io.File(presentDirectory)
+    if(presentDir.exists){
+      presentDir.delete;
+    }
+
+    FileUtils.copyDirectry(new java.io.File(deployRootCI), presentDir)
+    super.setDeployRoot(presentDirectory);
+
     if (sobjectPlural == true.toString) {
-      val dir = new java.io.File(getFileForPath(deployRootCI), "objects")
+      val dir = new java.io.File(getFileForPath(presentDirectory), "objects")
       println(dir)
       dir.listFiles().filter(_.getName.endsWith(".object")).foreach(file => {
         xml.PluralLabelCombine.combine(file.getCanonicalPath)
@@ -29,18 +37,22 @@ case class DeployTaskForCI extends DeployTask {
     this.deployRootCI = deployRoot
     super.setDeployRoot(deployRoot)
   }
+  var presentDirectory: String = "target/sforceci/src"
+  def setPresentDirectory(presentDirectory: String) = {
+    this.presentDirectory = presentDirectory
+  }
   var sobjectPlural: String = false.toString;
-  def setSobjectPlural(sobjectPlural: String){
+  def setSobjectPlural(sobjectPlural: String) {
     this.sobjectPlural = sobjectPlural;
   }
 
-  var coverageResultFile: String = null;
-  def setCoverageResultFile(coverageResultFile: String){
+  var coverageResultFile: String = "target/sforceci/coverage.xml";
+  def setCoverageResultFile(coverageResultFile: String) {
     this.coverageResultFile = coverageResultFile;
   }
 
-  var testResultFile: String = null;
-  def setTestResultFile(testResultFile: String){
+  var testResultFile: String = "target/sforceci/test-result.xml";
+  def setTestResultFile(testResultFile: String) {
     this.testResultFile = testResultFile;
   }
 
@@ -61,18 +73,10 @@ case class DeployTaskForCI extends DeployTask {
     val deployResult = metadataConnection.checkDeployStatus(response.getId());
 
     xml.JUnitXmlWriter.saveTestResult(
-      if (testResultFile == null) {
-        "target/sforceci/test-result.xml"
-      } else {
-        testResultFile
-      }, deployResult)
+      testResultFile, deployResult)
 
     xml.CoberturaXmlWriter.saveCoverageResult(
-      if (coverageResultFile == null) {
-        "target/sforceci/coverage.xml"
-      } else {
-        coverageResultFile
-      }, deployResult)
+      coverageResultFile, deployResult)
 
     if (deployResult.getMessages.exists(!_.isSuccess)) {
       super.handleResponse(metadataConnection, response);
---
+++ src/main/scala/com/yutagithub/sforce/ci/FileUtils.scala
@@ -0,0 +1,53 @@
+package com.yutagithub.sforce.ci
+
+import java.io._
+
+object FileUtils {
+  def using[A <% { def close(): Unit }](s: A)(
+    f: A => Any) {
+    try {
+      f(s)
+    } finally {
+      s.close
+    }
+  }
+  def copyFileBinaly(sFile: File, tFile: File): Unit = {
+    // ファイルの存在を確認
+    if (!sFile.exists()) return ;
+
+    var srcChannel = new FileInputStream(sFile).getChannel();
+    var destChannel = new FileOutputStream(tFile).getChannel();
+    try {
+      srcChannel.transferTo(0, srcChannel.size(), destChannel);
+    } finally {
+      srcChannel.close();
+      destChannel.close();
+    }
+  }
+
+  def copyDirectry(sDirectry: File, tDirectry: File): Unit = {
+    // コピー元がディレクトリでない場合はfalseを返す
+    if (!sDirectry.exists() || !sDirectry.isDirectory()) {
+      return ;
+    }
+    // ディレクトリを作成する
+    tDirectry.mkdirs();
+    // ディレクトリ内のファイルをすべて取得する
+    var files = sDirectry.listFiles();
+
+    // ディレクトリ内のファイルに対しコピー処理を行う
+    for (f <- files) {
+      if (f.isDirectory()) {
+        // ディレクトリだった場合は再帰呼び出しを行う
+        copyDirectry(
+          new File(sDirectry.toString(), f.getName()),
+          new File(tDirectry.toString(), f.getName()));
+      } else {
+        // ファイルだった場合はファイルコピー処理を行う
+        copyFileBinaly(
+          new File(sDirectry.toString(), f.getName()),
+          new File(tDirectry.toString(), f.getName()));
+      }
+    }
+  }
+}
